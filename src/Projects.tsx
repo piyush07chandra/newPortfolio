c30a13d2225e3c16c7907c92087ffecdcb555b30
piyush <piyushchandra9458@gmail.com>
2024-05-10 20:22:16 -1200

    third commit

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -14,7 +14,7 @@ const Projects:React.FC<clrType> = ({clr}) => {
   <h1 className="mt-8 mb-5">Projects( );</h1>
     </motion.div>
 {/* first */}
-      <div className="bg-gray-700 lg:m-12 m-7 lg:p-4 p-1 rounded-md shadow-lg shadow-white h-auto">
+      <div className="bg-gray-900 lg:m-12 m-7 lg:p-4 p-1 rounded-md shadow-lg shadow-white h-auto">
         <h1 className="pt-3 mb-8">Biorhythm App</h1>
         <p className="lg:text-2xl lg:block hidden mb-4">Track your physical, emotional, and intellectual cycles to optimize your life!</p>
         <h1 className="mx-auto justify-center flex "><a href="https://biorhythmm.netlify.app/" target="_blank" className="lg:text-3xl text-lg text-blue-400 hover:text-white flex">See the live site -  <img src="/right_1635581.png" className="lg:h-11 lg:w-20 h-10 w-10 "/></a></h1>
@@ -23,7 +23,7 @@ const Projects:React.FC<clrType> = ({clr}) => {
         <img  className="flex justify-center mx-auto rounded-md  shadow-lg shadow-yellow-500 mb-9" src="/Screenshot (33).png" width={700}/>
       </div>
   {/* second */}
-  <div className="bg-gray-700 lg:m-12 m-7 lg:p-4 p-1 rounded-lg shadow-md shadow-white">
+  <div className="bg-gray-900 lg:m-12 m-7 lg:p-4 p-1 rounded-lg shadow-md shadow-white">
         <h1 className="pt-3 mb-8">Demo Website</h1>
         <p className="lg:text-2xl lg:block hidden mb-4">From responsive design to intuitive navigation, we aim to make your browsing experience seamless. Whether you're on a desktop, tablet, or mobile device, our website adapts to ensure optimal viewing.</p>
         <h1 className="mx-auto justify-center flex"><a href="https://task3banao.netlify.app/" target="_blank" className="lg:text-3xl text-lg text-blue-400 hover:text-white flex">See the live site -  <img src="/right_1635581.png" className="lg:h-11 lg:w-20 h-10 w-10 "/></a></h1>
@@ -32,7 +32,7 @@ const Projects:React.FC<clrType> = ({clr}) => {
         <img  className="flex justify-center mx-auto rounded-md  shadow-lg shadow-purple-800 mb-9" src="/Screenshot (36).png" width={700}/>
       </div>
       {/* third */}
-      <div className="bg-gray-700 lg:m-12 m-7 lg:p-4 p-1 rounded-md shadow-lg shadow-white">
+      <div className="bg-gray-900 lg:m-12 m-7 lg:p-4 p-1 rounded-md shadow-lg shadow-white">
         <h1 className="pt-3 mb-8">Demo Website</h1>
         <p className="lg:text-2xl lg:block hidden mb-4">From responsive design to intuitive navigation, we aim to make your browsing experience seamless. Whether you're on a desktop, tablet, or mobile device, our website adapts to ensure optimal viewing.</p>
         <h1 className="mx-auto justify-center flex"><a href="https://banao2.netlify.app/" target="_blank" className="lg:text-3xl text-lg text-blue-400 hover:text-white flex">See the live site -  <img src="/right_1635581.png" className="lg:h-11 lg:w-20 h-10 w-10 "/></a></h1>
@@ -41,7 +41,7 @@ const Projects:React.FC<clrType> = ({clr}) => {
         <img  className="flex justify-center mx-auto rounded-md  shadow-lg shadow-white mb-9" src="/Screenshot (35).png" width={700}/>
       </div>
       {/* forth */} 
-      <div className="bg-gray-700 lg:m-12 m-7 lg:p-4 p-1 rounded-md shadow-lg shadow-white">
+      <div className="bg-gray-900 lg:m-12 m-7 lg:p-4 p-1 rounded-md shadow-lg shadow-white">
         <h1 className="pt-3 mb-8">Demo website</h1>
         <p className="lg:text-2xl lg:block hidden mb-4">From responsive design to intuitive navigation, we aim to make your browsing experience seamless. Whether you're on a desktop, tablet, or mobile device, our website adapts to ensure optimal viewing.</p>
         <h1 className="mx-auto justify-center flex"><a href="https://edzorb.netlify.app/" target="_blank" className="lg:text-3xl text-lg text-blue-400 hover:text-white flex">See the live site -  <img src="/right_1635581.png" className="lg:h-11 lg:w-20 h-10 w-10 "/></a></h1>
@@ -50,7 +50,7 @@ const Projects:React.FC<clrType> = ({clr}) => {
         <img  className="flex justify-center mx-auto rounded-md  shadow-lg shadow-white mb-9" src="/Screenshot (34).png" width={700}/>
       </div>
       {/* fifth */}
-      <div className="bg-gray-700 lg:m-12 m-7 lg:p-4 p-1 rounded-md shadow-lg shadow-white">
+      <div className="bg-gray-900 lg:m-12 m-7 lg:p-4 p-1 rounded-md shadow-lg shadow-white">
         <h1 className="pt-3 mb-8">Weather app</h1>
         <p className="lg:text-2xl lg:block hidden mb-4">Weather Now is your go-to app for up-to-date weather information. With our app, you can quickly check the current weather conditions and more for any location. Whether you're planning a trip, getting ready for your day, or just curious about the weather around the world, Weather Now has you covered.</p>
         <h1 className="mx-auto justify-center flex"><a href="https://openweatherr.netlify.app/" target="_blank" className="lg:text-3xl text-lg text-blue-400 hover:text-white flex">See the live site -  <img src="/right_1635581.png" className="lg:h-11 lg:w-20 h-10 w-10 "/></a></h1>
